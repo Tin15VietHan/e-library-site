32034ec493110cddb34d18254f01de56b00e733e
Tin15VietHan <nkaquoc2002@gmail.com>
2024-05-19 21:28:28 +0700

    oko

--- admin/sua_sach.php
+++ admin/sua_sach.php
@@ -35,10 +35,10 @@ function sanitize_input($data) {
 
 
     // $sql_post = "SELECT * FROM posts";
-    $sql_category= "SELECT * FROM categories";
-    $sql_post = "SELECT * FROM posts";
+    $sql_category= "SELECT * FROM khoa";
+    $sql_post = "SELECT * FROM sach";
 
-    $sql_up = "SELECT * FROM posts WHERE id = $id";
+    $sql_up = "SELECT * FROM sach WHERE id = $id";
     $query_up = mysqli_query($conn, $sql_up);
     $row_up = mysqli_fetch_assoc($query_up);
     if(!$row_up){
@@ -48,25 +48,25 @@ function sanitize_input($data) {
     $query_category = mysqli_query($conn, $sql_category);
     if(isset($_POST['sbm'])){
 
-        $title = $_POST['title'];
+        $title = $_POST['tensach'];
         
-        $image = $_FILES['image']['name'];
-        $image_tmp = $_FILES['image']['tmp_name'];
+        $image = $_FILES['anh']['tenkhoa'];
+        $image_tmp = $_FILES['anh']['tmp_tenkhoa'];
         if(!empty($image_tmp)){
-            move_uploaded_file($image_tmp, '../../image/'.$image);
+            move_uploaded_file($image_tmp, '../../anh/'.$image);
         }
 
         
-        $content = $_POST['content'];
+        $content = $_POST['gioithieusach'];
         
-        $category_id = $_POST['category_id'];
+        $category_id = $_POST['khoaID'];
 
         $status = $_POST['status'];
 
         $account = $_POST['taikhoanadmin_id'];
         $taikhoanadmin_id = (int)$account;
 
-        $sql = "UPDATE posts SET title ='$title', image = '$image', content = '$content', category_id= $category_id,status= '$status',  taikhoanadmin_id= $taikhoanadmin_id, updated_at = CURRENT_TIMESTAMP() where id = $id";
+        $sql = "UPDATE sach SET tensach ='$title', anh = '$image', gioithieusach = '$content', khoaID= $category_id,status= '$status',  taikhoanadmin_id= $taikhoanadmin_id, updated_at = CURRENT_TIMESTAMP() where id = $id";
         
         $query = mysqli_query($conn, $sql);
        
@@ -98,24 +98,24 @@ function sanitize_input($data) {
 
                     <div class="form-group">
                         <label for="">Tiêu đề</label>
-                        <input type="text" name="title" class="form-control" require value="<?php echo $row_up['title'] ?>">
+                        <input type="text" name="tensach" class="form-control" require value="<?php echo $row_up['tensach'] ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="">Ảnh bài viết</label>
-                        <input type="file" name="image" class="form-control">
+                        <input type="file" name="anh" class="form-control">
                     </div>
     
                     <div class="form-group">
                         <label for="">Nội dung</label>
-                        <textarea name="content" class="form-control form-control-size" id="content" value="" require><?php echo $row_up['content'] ?></textarea>
+                        <textarea name="content" class="form-control form-control-size" id="content" value="" require><?php echo $row_up['gioithieusach'] ?></textarea>
                     </div>
 
                     <div class="form-group">
                         <label for="">Thể loại</label>
-                        <select name="category_id" id="category_id">
+                        <select name="khoaID" id="khoaID">
                             <?php while ($row = mysqli_fetch_assoc($query_category)):?>
-                                <option value=<?php echo $row['id'];?> <?php if($row['id'] == $row_up['category_id']) echo 'checked';?>> <?php echo $row['name'];?></option>
+                                <option value=<?php echo $row['id'];?> <?php if($row['id'] == $row_up['khoaID']) echo 'checked';?>> <?php echo $row['tenkhoa'];?></option>
                             <?php endwhile;?>
                         </select>
                     </div>
